b1b8f7bf124cb69cc9365b774e303c3f6d87d8c1
AM-Ankitgit <ankitmahalle007@gmail.com>
2025-06-21 18:06:42 +0530

    File added

--- LangChain/08_Text_Splitters/03_WebBase_Loader.py
+++ LangChain/08_Text_Splitters/03_WebBase_Loader.py
@@ -1,3 +1,8 @@
 from langchain_community.document_loaders import WebBaseLoader
 url = "https://github.com/campusx-official/langchain-document-loaders/blob/main/webbase_loader.py"
-doc = WebBaseLoader(url)
+# url = "https://www.cricbuzz.com/"
+# doc = WebBaseLoader(url)
+
+url2 = 'https://www.flipkart.com/apple-macbook-air-m2-16-gb-256-gb-ssd-macos-sequoia-mc7x4hn-a/p/itmdc5308fa78421'
+loader = WebBaseLoader([url,url2])
+print(loader.load())
